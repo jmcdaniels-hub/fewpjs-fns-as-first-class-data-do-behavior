3694db57bb2f328734030bd7265bb6d2bf6ed3b0
Jade McDaniels <jademcdaniels81@gmail.com>
2021-02-23 16:31:39 -0500

    commit tree c494f571d667e261b94a4a6121814b1626d95b7b

--- index.js
+++ index.js
@@ -9,5 +9,29 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+// function greet(time=""){
+//   if (time < '11:59'){
+//     return "Good Morning";
+//   }
+//   else if (time >= '12:00' && time < '17:00'){
+//     return "Good Afternoon";
+//   } else if (time > '17:01' && time < '24:00'){
+//     return "Good Evening";
+//   }
+
+// }
+function greet(timeStr){
+  let nowTime = timeStr.split(':');
+  let nowHour = parseInt(nowTime[0]);
+  if (nowHour<12){
+    return "Good Morning";
+  } else if (nowHour>17){
+    return "Good Evening";
+  }
+  return "Good Afternoon";
+}
+
+
+function displayMessage(display){
+  document.getElementById('greeting').innerText = display
+}
